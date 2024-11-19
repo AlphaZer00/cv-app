4185a35d5f9e208277fdf92fb4d422673b8c043a
AlphaZer00 <batudasdemir@gmail.com>
2024-11-19 11:32:50 -0500

    Add edit and view modes to this section

--- src/components/GeneralInfo.jsx
+++ src/components/GeneralInfo.jsx
@@ -1,4 +1,10 @@
+import { useState } from "react";
+
 export default function GeneralInfo({ generalInfo, setGeneralInfo }) {
+    // Set default state of isEditing to true, this controls conditional rendering of edit and view modes
+    const [isEditing, setIsEditing] = useState(true);
+
+    // Handler function that updates state when any change is made to input values
     const handleChange = (e) => {
         const { name, value } = e.target;
 
@@ -9,39 +15,58 @@ export default function GeneralInfo({ generalInfo, setGeneralInfo }) {
         setGeneralInfo([updatedGeneralInfo]);
     };
 
+    // onClick function for submit button
+    const handleSubmit = () => setIsEditing(false);
+    // onClick function for edit button
+    const handleEdit = () => setIsEditing(true);
+
     return (
-        <div className="general-info-form">
+        <div className="general-info-section">
             <h3>General Information</h3>
-            <div className="gi-name">
-                <label htmlFor="name">Name</label>
-                <input
-                    type="text"
-                    id="name"
-                    name="name"
-                    value={generalInfo[0]?.name || ""}
-                    onChange={handleChange}
-                ></input>
-            </div>
-            <div className="gi-email">
-                <label htmlFor="email">Email</label>
-                <input
-                    type="email"
-                    id="email"
-                    name="email"
-                    value={generalInfo[0]?.email || ""}
-                    onChange={handleChange}
-                ></input>
-            </div>
-            <div className="gi-phone">
-                <label htmlFor="phone">Phone Number</label>
-                <input
-                    type="tel"
-                    id="phone"
-                    name="phone"
-                    value={generalInfo[0]?.phone || ""}
-                    onChange={handleChange}
-                ></input>
-            </div>
+            {/* If editing mode is ON for this section, render the form */}
+            {isEditing ? (
+                <div className="general-info-form">
+                    <div className="gi-name">
+                        <label htmlFor="name">Name: </label>
+                        <input
+                            type="text"
+                            id="name"
+                            name="name"
+                            value={generalInfo[0]?.name || ""}
+                            onChange={handleChange}
+                        ></input>
+                    </div>
+                    <div className="gi-email">
+                        <label htmlFor="email">Email: </label>
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            value={generalInfo[0]?.email || ""}
+                            onChange={handleChange}
+                        ></input>
+                    </div>
+                    <div className="gi-phone">
+                        <label htmlFor="phone">Phone Number: </label>
+                        <input
+                            type="tel"
+                            id="phone"
+                            name="phone"
+                            value={generalInfo[0]?.phone || ""}
+                            onChange={handleChange}
+                        ></input>
+                    </div>
+                    <button onClick={handleSubmit}>Submit</button>
+                </div>
+            ) : ( 
+                // If view mode is on, hide the form fields and only display the values
+                <div className="general-info-display">
+                    <p>Name: {generalInfo[0].name}</p>
+                    <p>Email: {generalInfo[0].email}</p>
+                    <p>Phone: {generalInfo[0].phone}</p>
+                    <button onClick={handleEdit}>Edit</button>
+                </div>
+            )}
         </div>
     );
 }
